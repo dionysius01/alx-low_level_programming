9552a8cbbc62e6979e240e23d63b7b084b555bd1
Dion <sealmaster30@gmail.com>
2022-12-08 18:41:35 -0800

    this is the last one

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,8 +1,11 @@
 #include<stdio.h>
-
+#include<unistd.h>
+/**
+ * main - entry
+ * Return: 1 (ERROR)
+ */
 int main(void)
 {
-
-	printf("and that piece of art is useful\" -Dora Korpar, 2015-10-19\n");
-	return 1;
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+return (1);
 }

1559602f52453dc46bae7a0685533936373fd923
Dion <sealmaster30@gmail.com>
2022-12-09 19:37:38 -0800

    commit

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - prints the alphabet in lowercase,
+ * followed by a new line
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alph;
+
+	for (alph = 'a'; alph <= 'z'; alph++)
+	{
+	    if(alph == 'e'|| alph == 'q')
+        {
+            continue;
+        }
+		putchar(alph);
+        putchar('\n');
+	}
+	return (0);
+}

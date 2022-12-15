b654600af36ecd2636f65cdc8a7994b923e94399
Dion <113058010+dionysius01@users.noreply.github.com>
2022-12-16 00:31:54 +0100

    d

--- 0x03-debugging/main.c
+++ 0x03-debugging/main.c
@@ -1,15 +1,16 @@
-#include "main.h"
+#include"main.h"
 
 /**
- * main - tests functions that prints if integer is postive or negative
- * Return: 0
- */
+* main - tests function that prints if integer is positive or negative
+* Return: 0
+*/
 
 int main(void)
 {
 	int i;
+
 	i = 98 % 2;
 	positive_or_negative(i);
 
-	return 0;
+	return (0);
 }

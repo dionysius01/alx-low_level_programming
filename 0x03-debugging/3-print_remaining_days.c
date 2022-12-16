8019d50c75d312b79782122bc75192233902bf33
Dion <113058010+dionysius01@users.noreply.github.com>
2022-12-16 01:14:23 +0100

    done

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -9,59 +9,33 @@
 * @year: year
 */
 
-int main(void)
+void print_remaining_days(int month, int day, int year)
 {
-    int day, mon, year, days_in_feb = 28,
-            doy;    // day of year
+	/**
+	 * leap year when it can be divided
+	 * by 100 and 400 or by 4 evenly
+	*/
+	if ((year % 100 == 0 && year % 400 == 0) || (year % 4 == 0))
+	{
+		if (month > 2 && day >= 60)
+		{
+			day++;
+		}
 
-    printf("Enter date (MM/DD/YYYY): ");
-    scanf("%d/%d/%d", &mon, &day, &year);
+		printf("Day of the year: %d\n", day);
+		printf("Remaining days: %d\n", 366 - day);
 
-    doy = day;
-
-    // check for leap year
-    if( (year % 4 == 0 && year % 100 != 0 ) || (year % 400 == 0) )
-    {
-        days_in_feb = 29;
-    }
-
-    switch(mon)
-    {
-        case 2:
-            doy += 31;
-            break;
-        case 3:
-            doy += 31+days_in_feb;
-            break;
-        case 4:
-            doy += 31+days_in_feb+31;
-            break;
-        case 5:
-            doy += 31+days_in_feb+31+30;
-            break;
-        case 6:
-            doy += 31+days_in_feb+31+30+31;
-            break;
-        case 7:
-            doy += 31+days_in_feb+31+30+31+30;
-            break;
-        case 8:
-            doy += 31+days_in_feb+31+30+31+30+31;
-            break;
-        case 9:
-            doy += 31+days_in_feb+31+30+31+30+31+31;
-            break;
-        case 10:
-            doy += 31+days_in_feb+31+30+31+30+31+31+30;
-            break;
-        case 11:
-            doy += 31+days_in_feb+31+30+31+30+31+31+30+31;
-            break;
-        case 12:
-            doy += 31+days_in_feb+31+30+31+30+31+31+30+31+30;
-            break;
-    }
-
-    printf("Day of year: %d", doy);
-
-    return 0;
+	}
+	else
+	{
+		if (month == 2 && day == 60)
+		{
+			printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
+		}
+		else
+		{
+			printf("Day of the year: %d\n", day);
+			printf("Remaining days: %d\n", 365 - day);
+		}
+	}
+}

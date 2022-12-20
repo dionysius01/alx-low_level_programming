7da41c7915b3af02f7607edc1008855c8ab766ae
Dion <sealmaster30@gmail.com>
2022-12-19 18:37:47 -0800

    :

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,5 +1,12 @@
 #include "main.h"
-
+/**
+ * swap_int - swap to integer values
+ *
+ * @a: first value to swap
+ * @b: second value to swap
+ *
+ * Return: always void
+ */
 void swap_int(int *a, int *b)
 {
 	int p = *a;

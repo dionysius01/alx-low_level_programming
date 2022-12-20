6bbf9afe646e2802a909808389d85b054a3702da
Dion <sealmaster30@gmail.com>
2022-12-19 19:08:32 -0800

    done

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,26 @@
+#include"main.h"
+/**
+ * puts_half - print second half of str
+ *
+ * @str: string to cut in half & print
+ *
+ * Return: always void
+ */
+void puts_half(char *str)
+{
+	int l, n;
+
+	for (l = 0; str[l] != '\0'; ++l)
+		;
+
+	if (l % 2 == 0)
+	{
+		for (n = l / 2; str[n] != '\0'; ++n)
+			_putchar(str[n]);
+	} else
+	{
+		for (n = ((l - 1) / 2) + 1; str[n] != '\0'; ++n)
+			_putchar(str[n]);
+	}
+	_putchar('\n');
+}
